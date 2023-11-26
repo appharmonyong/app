5fe4e367b426d54d7a34f86781bd52d75d0eb1a3
Jose A. Dominique <josea.dominique01@gmail.com>
2023-11-26 16:49:27 -0400

    .

--- Harmony/Harmony/Controllers/HomeController.cs
+++ Harmony/Harmony/Controllers/HomeController.cs
@@ -11,18 +11,35 @@ namespace Harmony.Presentation.Main.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IUserServices _authServices;
 
+        private readonly IWebHostEnvironment _env;
 
-        public HomeController(ILogger<HomeController> logger, IUserServices authServices)
+        public HomeController(ILogger<HomeController> logger, IUserServices authServices, IWebHostEnvironment env)
         {
             _logger = logger;
             _authServices = authServices;
+            _env = env;
         }
 
         public async Task<IActionResult> Index()
         {
             var users = await _authServices.Get();
 
-            return View(users);
+            //Insertar las fotos que irán en el carousel aquí
+            var carouselItems = new List<CarouselItem>
+            {
+                new CarouselItem {ImageUrl = "https://vibes.okdiario.com/wp-content/uploads/2023/07/pension-seguridad-social-amas.jpg", CaptionTitle = "Emplead@s", CaptionBody = "Nuestros empleados verificados", HasButtons = true},
+                new CarouselItem {ImageUrl = "https://whaticket.com/wp-content/uploads/2022/06/clientes-satisfechos-1-1024x577.png", CaptionTitle = "Servicios", CaptionBody = "Ofrecemos los mejores servicios", HasButtons = false},
+                new CarouselItem {ImageUrl = _env.WebRootFileProvider.GetFileInfo("images/harmony1.jpg")?.PhysicalPath, CaptionTitle = "Harmony", CaptionBody = "Cuenta con nosotros", HasButtons = false}
+            };
+
+            //Carga los datos para mandarlo a vista
+            var viewModel = new IndexViewModel
+            {
+                Users = (List<UserVm>)users,
+                CarouselItems = carouselItems
+            };
+
+            return View(viewModel);
         }
 
         [Authorize]
