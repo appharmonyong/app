ea627d7915ab5b2d9c8c3fb289f75500d5200022
Gilberto Ubiera <joumar.rafael19@gmail.com>
2023-12-01 05:55:31 +0100

    Creación del footer o información de contacto junto con algunos cambios en el carousel. -Commit-

--- Harmony/Harmony/Controllers/HomeController.cs
+++ Harmony/Harmony/Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace Harmony.Presentation.Main.Controllers
             {
                 new CarouselItem {ImageUrl = "https://vibes.okdiario.com/wp-content/uploads/2023/07/pension-seguridad-social-amas.jpg", CaptionTitle = "Emplead@s", CaptionBody = "Nuestros empleados verificados", HasButtons = true},
                 new CarouselItem {ImageUrl = "https://whaticket.com/wp-content/uploads/2022/06/clientes-satisfechos-1-1024x577.png", CaptionTitle = "Servicios", CaptionBody = "Ofrecemos los mejores servicios", HasButtons = false},
-                new CarouselItem {ImageUrl = "~/images/harmony1.jpg", CaptionTitle = "Harmony", CaptionBody = "Cuenta con nosotros", HasButtons = false}
+                new CarouselItem {ImageUrl = "https://plus.unsplash.com/premium_photo-1664910908445-c902d8b80b8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHB1ZWJsbyUyMGphcG9uZXN8ZW58MHx8MHx8fDE%3D&w=1000&q=80", CaptionTitle = "Harmony", CaptionBody = "Cuenta con nosotros", HasButtons = false}
             };
 
             //Carga los datos para mandarlo a vista
