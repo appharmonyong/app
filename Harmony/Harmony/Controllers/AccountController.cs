f9240386ef6f1fa28c0b0289972b0bd502971fea
Jose A. Dominique <josea.dominique01@gmail.com>
2023-12-12 10:10:59 -0400

    perfil cuenta

--- Harmony/Harmony/Controllers/AccountController.cs
+++ Harmony/Harmony/Controllers/AccountController.cs
@@ -9,16 +9,42 @@ namespace Harmony.Presentation.Main.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly IUserServices _userService;
+        private readonly IUserServices _userServices;
 
         public AccountController(IUserServices userService)
         {
-            _userService = userService;
+            _userServices = userService;
+        }
+
+        private async Task UserLoginOrAdmin()
+        {
+            if (await IsUserLogin())
+            {
+
+                TempData["IsUserLogIn"] = "Auth";
+            }
+            else
+            {
+                TempData["IsUserLogIn"] = null;
+            }
+
+
+            if (await IsUserAdmin())
+            {
+
+                TempData["IsUserAdmin"] = "IsUserAdmin";
+            }
+            else
+            {
+                TempData["IsUserAdmin"] = null;
+            }
+
         }
 
         [HttpGet]
         public async Task<IActionResult> LogOut()
         {
+            await UserLoginOrAdmin();
             if (HttpContext.Session.Get("UserId") != null)
             {
                 HttpContext.Session.Remove("UserId");
@@ -30,6 +56,7 @@ namespace Harmony.Presentation.Main.Controllers
         [HttpGet]
         public async Task<IActionResult> Login()
         {
+            await UserLoginOrAdmin();
             if (HttpContext.Session.GetInt32("UserId") != null)
             {
                 return RedirectToAction("Index", "Home");
@@ -40,11 +67,12 @@ namespace Harmony.Presentation.Main.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(UserLogInVm model)
         {
+            await UserLoginOrAdmin();
             if (ModelState.IsValid)
             {
                 try
                 {
-                    var user = await _userService.SignIn(model);
+                    var user = await _userServices.SignIn(model);
 
                     // Autenticación exitosa, puedes realizar acciones adicionales si es necesario.
 
@@ -66,17 +94,19 @@ namespace Harmony.Presentation.Main.Controllers
             return View("Login", model);
         }
 
-        
+
         // Método para manejar el registro
         [HttpGet]
-        public IActionResult Register()
+        public async Task<IActionResult> Register()
         {
+            await UserLoginOrAdmin();
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> Register(UserRegisterVm model)
         {
+            await UserLoginOrAdmin();
             try
             {
                 if (!ModelState.IsValid)
@@ -84,7 +114,7 @@ namespace Harmony.Presentation.Main.Controllers
                     return View(model);
                 }
                 model.userType = Common.EUserTypes.Customer;
-                await _userService.Register(model);
+                await _userServices.Register(model);
 
                 TempData["Success"] = "Usuario creado con exito.";
                 return View();
@@ -95,6 +125,94 @@ namespace Harmony.Presentation.Main.Controllers
                 return RedirectToAction("CreateAdmin");
             }
         }
+
+        [HttpGet]
+        public async Task<IActionResult> MyAccount()
+        {
+            await UserLoginOrAdmin();
+            int userId = HttpContext.Session.GetInt32("UserId") ?? 0;
+
+            var user = await _userServices.GetById(userId);
+            return View(user);
+        }
+
+
+
+        public async Task<bool> IsUserLogin()
+        {
+            try
+            {
+
+                int userId = HttpContext.Session.GetInt32("UserId") ?? 0;
+
+                if (userId != 0)
+                {
+                    var user = await _userServices.GetById(userId);
+                    return user != null;
+                }
+            }
+            catch
+            {
+            }
+
+            return false;
+        }
+
+
+
+        public async Task<bool> IsUserAdmin()
+        {
+            if (await IsUserLogin())
+            {
+
+                int userId = HttpContext.Session.GetInt32("UserId") ?? 0;
+
+                var user = await _userServices.GetById(userId);
+
+                return user.UserType == Common.EUserTypes.Administrator;
+            }
+
+            return false;
+        }
+
+
+        public async Task<IActionResult> UpdateAdmin(UserVm model, string? actionRedirect = "MyAccount")
+        {
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                await _userServices.Update(model.Id, new UserUpdateVm()
+                {
+                    Id = model.Id,
+                    BirthDay = model.BirthDay,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Phone = model.Phone
+                });
+                TempData["Success"] = "Usuario actualizado con exito.";
+                return RedirectToAction(actionRedirect, "Account");
+            }
+            catch (ArgumentException ex)
+            {
+                TempData["Error"] = ex.Message;
+                return RedirectToAction(actionRedirect);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateUser(UserVm user)
+        {   
+            await UpdateAdmin(user, "MyAccount");
+            return RedirectToAction("MyAccount", "Account");
+
+
+        }
+
     }
 }
 
